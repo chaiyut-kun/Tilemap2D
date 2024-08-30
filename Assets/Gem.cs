deef5c4a5177596e9f565727df01f998d42ee142
chaiyut-kun <chwork28@hotmail.com>
2024-08-30 10:23:40 +0700

    Something

---
+++ Assets/Gem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            // GetComponent<AudioSource>.Player();
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+            // Destroy(col);
+        }
+    }    
+}
