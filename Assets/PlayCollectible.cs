0bffdd42f1b7e009847242eb8c1bbba5528af118
chaiyut-kun <chwork28@hotmail.com>
2024-09-06 10:07:53 +0700

    Add PlayCollectible.cs for counting gem collected

---
+++ Assets/PlayCollectible.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayCollectible : MonoBehaviour
+{
+    private int gem_number = 0;
+    public Text text_component;
+    // Start is called before the first frame update
+    private void UpdateText(){
+        text_component.text = gem_number.ToString();
+    }
+    void Start()
+    {
+        UpdateText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
