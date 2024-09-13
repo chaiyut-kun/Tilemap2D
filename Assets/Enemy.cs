dfad7095542c98111cd180baba1525f9126a8c2c
chaiyut-kun <chwork28@hotmail.com>
2024-09-13 15:59:04 +0700

    Create enemy and enemy movement

---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health;
+    protected Rigidbody2D rb;
+    protected Animator anim;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
